7a9d7660fcc0f0e5c25a04f5ebeeafaae1c87b0a
unknown <joseph.scalera1@gmail.com>
2022-05-08 21:16:25 -0400

    final version

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # petblog
+
+Install:
+`npm instal`
+
+Seed:
+`npm run seed`
+
+Deploy:
+`npm start`
+
+
+
+We used the following libraries:
+- bcryptjs for password hashing and constant time string comparisons
+- crypto for generating random passwords for the sessions
+- dotenv for loading the database URI
+- express-handlebars for the frontend
+- express-session for handeling the login sessions
+- fs for writing the images uploaded to the local filesystem
+- multer for processing file uploads over http
+- path for properly joining the current working directory with a specific path
+- uuid for generating a random identifier for the uploaded images.
--- seed/restore.js
+++ seed/restore.js
@@ -7,15 +7,61 @@ const petsCollection = require("../config/mongoCollections").pets
 
 const seed = async function () {
     const accounts = await accountsCollection();
+
+    // Password is 1234
     await accounts.insertOne({
-        "username": "admin",
-        "hashpass": "$2a$10$B7iLM6nEdSgFuNrAU2FVyu7EmB3MJkEnXyb0G10dNyhJRDn2XxIqi",
+        "username": "user",
+        "hashpass": "$2a$10$WiMPiekG6eF3ehudpPIj2.yuMhCJhRFgP3CpTSxDD2yk9kKDn6vh6",
         "name": null,
         "bio": null,
         "picture": null
     });
 
-    
+    // Password is 1234
+    await accounts.insertOne({
+        "username": "user1",
+        "hashpass": "$2a$10$aTA/t9LS0qbRe/Qo6Xe1D.k1yXPuLp0JL0TjcYRDujZpfjOkVvpyK",
+        "name": null,
+        "bio": null,
+        "picture": null
+    });
+
+    const pets = await petsCollection();
+    await pets.insertOne({
+        "username": "user1",
+        "animalName": "Adam",
+        "animalType": "dog",
+        "animalAge": 2,
+        "zipcode": "07053",
+        "description": "adam is a very funny pet",
+        "tag": "funny",
+        "image": "0ae88b1b20944ffdecbe8bf86c9011a4",
+        "priorOwners": [
+            "user"
+        ]
+    })
+
+    await pets.insertOne({
+        "username": "user",
+        "animalName": "Joe",
+        "animalType": "cat",
+        "animalAge": 1,
+        "zipcode": "07946",
+        "description": "a very small cat",
+        "tag": "small",
+        "image": "0ae88b1b20944ffdecbe8bf86c9011a4",
+        "priorOwners": []
+    })
+
+    const posts = await postsCollection();
+    await posts.insertOne({
+        "username": "user",
+        "title": "My new pet",
+        "image": "0ae88b1b20944ffdecbe8bf86c9011a4",
+        "tag": "pet",
+        "body": "here is my new pet",
+        "date": 1652057697
+    })
 
     process.exit();
 }
